1d6f361d158c82d97e8fb9cd978019815f331a53
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-08-18 09:39:58 -0500

    Installed ejs, made link to index view

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const { indexRouter: indexRouter } = require("./routes/indexRouter");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/", indexRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-  console.log(`My first Express app - listening on port ${PORT}!`);
+  console.log(`Inventory app launched - listening on port ${PORT}!`);
 });
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,14 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+indexRouter.get("/:category", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
+
+indexRouter.get("/:item", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
+
 module.exports = { indexRouter };
