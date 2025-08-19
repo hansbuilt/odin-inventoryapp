7175521a909f67162c368261bdb8f1aa44463c46
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-08-19 09:09:50 -0500

    Added initial page rendering/styling

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-const { indexRouter: indexRouter } = require("./routes/indexRouter");
+const {
+  indexRouter: indexRouter,
+  itemData,
+  catData,
+} = require("./routes/indexRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { catData: catData });
 });
 
 const PORT = 3000;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,24 @@
 const { Router } = require("express");
+
+const itemData = {
+  1: {
+    id: "1",
+    sku: "test",
+    product_name: "test_name",
+    product_description: "test_desc test_desc test_desc test_desc",
+    category_id_fk: "22",
+    price: "$9.99",
+    product_image: "TBD",
+  },
+};
+const catData = [
+  {
+    id: "22",
+    category_name: "test_cat22",
+  },
+  { id: "33", category_name: "test_cat33" },
+];
+
 const indexRouter = Router();
 
 indexRouter.get("/category/:category", (req, res) => {
@@ -6,7 +26,13 @@ indexRouter.get("/category/:category", (req, res) => {
 });
 
 indexRouter.get("/item/:item", (req, res) => {
-  res.render("item");
+  const itemID = req.params.item;
+  const item = itemData[itemID];
+  if (!item) {
+    return res.status(404).send("Item not found");
+  }
+
+  res.render("item", { item });
 });
 
-module.exports = { indexRouter };
+module.exports = { indexRouter, itemData, catData };
