b8db873ab06fae79edec269da0efa17a2dbb3043
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-08-20 11:17:47 -0500

    Added create, update, delete queries

--- db/queries.js
+++ db/queries.js
@@ -29,6 +29,81 @@ async function getSingleProduct(id) {
   return rows;
 }
 
+async function createSingleProduct({
+  sku,
+  product_name,
+  product_description,
+  category_id,
+  price,
+  product_image,
+}) {
+  const { rows } = await pool.query(
+    `INSERT INTO products (sku, product_name, product_description, category_id, price, product_image)
+  VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
+    [sku, product_name, product_description, category_id, price, product_image]
+  );
+  return rows[0];
+}
+
+async function createCategory({ category_name }) {
+  const { rows } = await pool.query(
+    `INSERT INTO categories (category_name) VALUES ($1) RETURNING *;`,
+    [category_name]
+  );
+  return rows[0];
+}
+
+async function updateProduct(
+  id,
+  { sku, product_name, product_description, category_id, price, product_image }
+) {
+  const { rows } = await pool.query(
+    `UPDATE products
+     SET sku = $1,
+         product_name = $2,
+         product_description = $3,
+         category_id = $4,
+         price = $5,
+         product_image = $6
+     WHERE id = $7
+     RETURNING *;`,
+    [
+      sku,
+      product_name,
+      product_description,
+      category_id,
+      price,
+      product_image,
+      id,
+    ]
+  );
+  return rows[0];
+}
+
+async function updateCategory(id, { category_name }) {
+  const { rows } = await pool.query(
+    `UPDATE categories
+     SET campaign_name = $1,
+         
+     WHERE id = $2
+     RETURNING *;`,
+    [category_name, id]
+  );
+  return rows[0];
+}
+
+async function deleteSingleProduct(id) {
+  const result = await pool.query("DELETE FROM products WHERE id = $1;", [id]);
+  return result.rowCount;
+}
+
+async function deleteSingleCategory(id) {
+  const result = await pool.query("DELETE FROM categories WHERE id = $1;", [
+    id,
+  ]);
+  return result.rowCount;
+}
+
 module.exports = {
   getAllCategories,
   getAllProductsOfCategory,
