6f5fe93770b988ff74707721c8775597280b4b57
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-08-25 09:51:23 -0500

    Updated to allow forms to be read

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const path = require("node:path");
 const {
   indexRouter: indexRouter,
   itemData,
   catData,
 } = require("./routes/indexRouter");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
